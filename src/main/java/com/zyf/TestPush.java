28d50ff2f6902c2d860b41951375be81a6ebe5b0
zhouyunting <zhouyunting@pcitech.com>
2021-09-28 10:49:47 +0800

    初始化工程

---
+++ src/main/java/com/zyf/TestPush.java
@@ -0,0 +1,11 @@
+package com.zyf;
+
+/**
+ * @author zyting
+ * @since 2021-09-28
+ */
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("初始化工程");
+    }
+}
