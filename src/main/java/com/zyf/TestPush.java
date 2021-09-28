85705274eda1fe7d5677fcad36300600cf7bd866
zhouyunting <zhouyunting@pcitech.com>
2021-09-28 11:44:23 +0800

    master修改代码

--- src/main/java/com/zyf/TestPush.java
+++ src/main/java/com/zyf/TestPush.java
@@ -8,5 +8,6 @@ public class TestPush {
     public static void main(String[] args) {
         System.out.println("初始化工程");
         System.out.println("dev提交代码");
+        System.out.println("master修改代码");
     }
 }
